e6dea85de615904972761466cd1c0d2945b82444
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-10-24 18:47:09 +0200

    fix

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/installation/intro">
+            Installation Party - 60min ⏱️
           </Link>
         </div>
       </div>
